7d599eb0a751b3dda3704643a2c162d07adb8f63
= <josebeto1113@gmail.com>
2023-03-29 13:56:12 -0300

    Include Pinia to project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+createApp(App).use(router).use(pinia).mount('#app')
